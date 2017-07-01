aa85681a6141079b69e37d5bf7dc7a2fb80a5ce2
dandrikevich <denis.andrikevich@gmail.com>
2017-07-02 00:12:25 +0300

    refactored loginAction

--- redux/login/LoginActions.js
+++ redux/login/LoginActions.js
@@ -1,5 +1,6 @@
 import http from '../../utils/http.service';
 import storage from '../../utils/storage.service';
+import serialize from '../../utils/serialize';
 import {
     LOGIN_REQUEST,
     LOGIN_SUCCESS,
@@ -8,10 +9,16 @@ import {
 } from './LoginConstants';
 
 export function loginAction(data, success, error) {
+    const header = {
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    }
+
     return dispatch => {
         dispatch({ type: LOGIN_REQUEST });
-
-        http.service.post('api/v2/mobile/login', data)
+        
+        http.service.post('api/v2/mobile/login', serialize(data), header)
             .then(res => {
                 storage.service.setItem('token', res.data.token);
                 dispatch({ type: LOGIN_SUCCESS, payload: res.data.token })
