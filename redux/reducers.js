ee965cb97f17977ad24e5cc6398bd88b61b4459d
dandrikevich <denis.andrikevich@gmail.com>
2017-07-01 13:48:20 +0300

    removed redux-router

--- redux/reducers.js
+++ redux/reducers.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import loginReducer from './login/LoginReducer';
 import { reducer as formReducer } from 'redux-form';
-import { routerReducer } from 'react-router-redux'
 
 
 const rootReducer = combineReducers({
     login: loginReducer,
-    form: formReducer,
-    routing: routerReducer
+    form: formReducer
 })
 
 export default rootReducer;
