037e3edf8711d6a6e37565c3623b7e55d034a7c4
dandrikevich <denis.andrikevich@gmail.com>
2017-07-03 21:29:23 +0300

    added comment for creating store

--- redux/store.js
+++ redux/store.js
@@ -7,6 +7,12 @@ import storage from '../utils/storage.service';
 
 let store;
 
+/**
+ * httpService interface (get,post,put,delete methods)
+ * storageService (setItem, removeItem methods)
+ * 
+ */
+
 export default (httpService, storageService) => {
     if (!httpService) throw new Error('Http service required');
     if (!storageService) throw new Error('Storage service requiered');
